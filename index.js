60feaa741522eb41bf4f779dec9c67e8b41481ba
Tolase Durojaye <tolasesaheed@yahoo.co.uk>
2018-12-11 21:04:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
+
+
 function dwarfRollCall(dwarves) {
-}
+ 	var myString = "";
+   for (var i = 0; i < dwarves.length; i++) {
+    myString += (i+1).toString()+ ". " + dwarves[i] + " ";
+   }
+   return myString;
+ }
+
+
 
 function summonCaptainPlanet(planeteerCalls){
+  	var calls = [];
+  for (var i = 0; i < planeteerCalls.length; i++) {
+   calls.push(planeteerCalls[i].toUpperCase() + "!");
+  }
+  return calls;
 }
 
 function longPlaneteerCalls(words) {
+  var Word = false;
+  for (var i = 0; i < words.length; i++){
+    if (words[i].length > 4){
+      Word = true;
+    
+  }
+
+}
+return Word;
 }
 
+
+
 function findTheCheese (foods) {
-}
+ 	var cheeses = ["cheddar", "gouda", "camembert"];
+ 	for (var i = 0; i < foods.length; i++) {
+ 		for (var j = 0; j < cheeses.length; j++) {
+ 			if ( cheeses[j]=== foods[i] ) {
+ 				return foods[i]; 
+ 			}
+ 		}
+ 	}
+ 	return "no cheese!"; 
+ }
+
+
+	
